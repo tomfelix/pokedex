249a9ebfec14086f88e1822e8c3c58c26c7e97dd
TomFelix <tomasz.agacinski@gmail.com>
2018-06-06 21:04:49 +0200

    added react-redux, provider to App component

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore } from 'redux';
 import rootReducer from './reducers';
+import Provider from 'react-redux';
 
 const store = createStore(rootReducer);
 console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
